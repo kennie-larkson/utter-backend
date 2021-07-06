6a28a7950b4dd4d78a76f88fca0159d004d52fae
kennie-larkson <materialkenny@gmail.com>
2021-07-06 18:45:52 +0200

    feat(cookies and sessions): implemented authentication and sessions with passport and express-sessions

--- components/users/userModel.js
+++ components/users/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose"
+import passport from "passport"
 
 const basicRegSchema = new mongoose.Schema({
   email: {
@@ -15,6 +17,8 @@ const basicRegSchema = new mongoose.Schema({
   },
 });
 
+basicRegSchema.plugin(passportLocalMongoose, {usernameField: "email"});
 const NewReg = mongoose.model("NewReg", basicRegSchema);
+
 
 export default NewReg;
